11084f3b2cae19a5518eb2b645ab49be16c64592
freeman.genie777@gmail.com <sonsus@github.com>
2023-09-02 18:19:53 +0900

    fixed postprocessing

--- src/prompts/plancode_util.py
+++ src/prompts/plancode_util.py
@@ -60,10 +60,17 @@ def add_indents2plan(plan:str)->str:
 
 def postprocess_code_answer(rawanswer:str, docdef:str=''):
     try:
+        # removing starting wrap ```
         if "```python" in rawanswer:
             code = rawanswer.split("```python")[1].split("```")[0]
-        else: # consider it continuates from prompt (starting with docstring)
-            code = rawanswer.split("```")[0] #
+        elif rawanswer.startswith('```'):
+            rawanswer = rawanswer.split('```')[-1]
+        # removing ``` at the end
+        code = rawanswer.split("```")[0] #ending ``` removal
+        # check if the code startswith docstring
+        if code.startswith(docdef) or code.startswith(docdef.replace('"""', "'''")):
+            pass
+        else: # docstring not contained
             if code.startswith('def solution():'):
                 code = "\n".join(code.split('\n')[1:]) # avoid def solution twice
             code = docdef+'\n'+code # when continuates from the prompt (containint def)
@@ -75,7 +82,7 @@ def postprocess_code_answer(rawanswer:str, docdef:str=''):
 
 def remove_prints(code:str)->str:
     lines = code.split("\n")
-    lines_ = [l for l in lines if not l.strip().startswith('print')]
+    lines_ = [l if not l.startswith('print(') else l.replace('print(', '# print(') for l in lines]
     code_ = "\n".join(lines_)
     return code_
 
@@ -129,10 +136,7 @@ def make_code_examples()->Sequence[str]:
         
         # print(code)
         # pack fewshots into list
-        code_rev =[(docstring_def if l.startswith('def solution():') and (docstring_def not in l) else l) for l in codeonly.split('\n')]
-        code_rev = "\n".join(code_rev)
-
-        code_shot = f"{q}\n# solution in Python:\n\n\n{code_rev}"
+        code_shot = f"{q}\n# solution in Python:\n\n\n{codeonly}"
         code_fewshots.append(code_shot)
         print('completed_fewshots')
         for fs in code_fewshots:
@@ -142,38 +146,40 @@ def make_code_examples()->Sequence[str]:
 
 
 if __name__ == '__main__':
+    prompt_dd = yaml.full_load(open('prompts_code_fewshot.yaml'))
+    print()
 
-    """data = {"question": "Question: What do you do for a living?"}
-    pp1 = get_plan_prompt(data, k_fewshot=1)
-    pp3 = get_plan_prompt(data, k_fewshot=3)
-    plan = '''1. Check how much money Olivia had at first, and store it into some variable.
-2. She's consuming it to buy bagels. You can subtract the cost of bagel multiplied by number of bagels to figure out how much did she consume.
-3. Subtract the cost from initial deposit.
-4. Return the calculated number.
-</end>'''
-    cp0 = get_plan2code_prompt(data, plan=plan)
+#     data = {"question": "Question: What do you do for a living?"}
+#     pp1 = get_plan_prompt(data, k_fewshot=1)
+#     pp3 = get_plan_prompt(data, k_fewshot=3)
+#     plan = '''1. Check how much money Olivia had at first, and store it into some variable.
+# 2. She's consuming it to buy bagels. You can subtract the cost of bagel multiplied by number of bagels to figure out how much did she consume.
+# 3. Subtract the cost from initial deposit.
+# 4. Return the calculated number.
+# </end>'''
+#     cp0 = get_plan2code_prompt(data, plan=plan)
     
-    def kvprint(record):
-        for r in record:
-            print(r['role'])
-            print(r['content'])
+#     def kvprint(record):
+#         for r in record:
+#             print(r['role'])
+#             print(r['content'])
 
-    print("pp3")
-    kvprint(pp3)
-    print("===============================")
-    print("pp1")
-    kvprint(pp1)
-    print("===============================")
-    print("cp0")
-    kvprint(cp0)
-    print("===============================")"""
+#     print("pp3")
+#     kvprint(pp3)
+#     print("===============================")
+#     print("pp1")
+#     kvprint(pp1)
+#     print("===============================")
+#     print("cp0")
+#     kvprint(cp0)
+#     print("===============================")
+#     # print(KEY);print(KEY)
+#     code_fewshots = make_code_examples()
+#     prompt_d = yaml.full_load(open(CODE_F))
+#     prompt_d.update({'fewshots': code_fewshots})
+#     with open('prompts_code_fewshot.yaml', 'w') as ymlf:
+#         yaml.dump(prompt_d, ymlf, default_flow_style=False)
 
-    # print(KEY);print(KEY)
-    code_fewshots = make_code_examples()
-    prompt_d = yaml.full_load(open(CODE_F))
-    prompt_d.update({'fewshots': code_fewshots})
-    with open('prompts_code_fewshot.yaml', 'w') as ymlf:
-        yaml.dump(prompt_d, ymlf, default_flow_style=False)
 
 
     
