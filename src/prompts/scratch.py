e055c07a2ba1e6e5300a1ec1a1597d53cb1381c2
freeman.genie777@gmail.com <sonsus@github.com>
2023-10-01 22:38:53 +0900

    TODO, miscellaneous util functions

--- src/prompts/scratch.py
+++ src/prompts/scratch.py
@@ -1,10 +1,19 @@
 import yaml
+from typing import Dict, List, Tuple, Any, Sequence, Mapping, Callable
+from fire import Fire
+
+# heuristic for unbiased sampling from trainingset
+import pandas as pd
+import jsonlines as jsl 
+from pathlib import Path
+import datasets
+
 ACTOR_PROMPTS = yaml.full_load('actor_prompt.yaml')
 
 def kshot_harvesting(
     data:Dict[str, Any],
     n_reflect:int = 1, # args.n_reflect --> per method, how many times to reflect?
-    first_reasoning_how:'random', #'llm' args.first_reasoning_how
+    first_reasoning_how:str='random', #'llm' args.first_reasoning_how
 ):
     '''
     Few-shot learning of the master LLM choosing a good method to tackle "a" problem. (constructs fewshot examples for master LLM model-choice)
@@ -71,14 +80,14 @@ def kshot_harvesting(
         # 5. prepare next round
         sys = ACTOR_PROMPTS['system_reflect']
         attempt = ACTOR_PROMPTS['attempt_reflect'].replace("{CANDIDS}", f"{list(set(models_to_sample))}").replace("{QUESTION}", question)
-        
 
-        user_msgs.append(attempt_i)
-        
+        assist_msgs.append(choice_reasoning)
+        user_msgs.append(attempt)
+        prom = prep_gpt_input(sys=sys, user_msgs=user_msgs, assist_msgs=assist_msgs)
+                
         
 def prep_reflection_prompt(sol, choice, pred):
     return ACTOR_PROMPTS['invoke_reflection'].replace('{SOL}', sol).replace('{CHOICE}', choice).replace('{PRED}', pred)
-
 
 
 def prep_gpt_input(
@@ -99,3 +108,59 @@ def prep_gpt_input(
         if i<len(assist_msgs):
             msgs.append({'role': 'assist', 'content': assist_msgs[i]})
     return msgs
+
+
+
+
+def get_k_train_shots(
+                k:int=10,
+                train_f:str='gsm8k_train.jsonl', 
+                heuristics:str='wordcount'
+                ): 
+    if heuristics == 'wordcount':
+        df = pd.DataFrame(jsl.open(train_f))
+        df['wordcount'] = df.question.apply(lambda q:len(q.split()))
+        df_ = df.sort_values(by='wordcount')
+        idxs = [i for i in range(0, len(df), len(df)//k)][:k]
+        resdf = df_.iloc[idxs]
+        kshots = resdf.to_dict(orient='records')
+    else:
+        raise NotImplementedError(f'heuristics {heuristics} not implemented.')
+    
+    return kshots # List[Dict[str,str]]
+
+# util for gsm8k train split download and parsing
+def gsm8k_train_download_and_parse(root:str='./'):
+    # check if exists
+    root = Path(root)
+    target_path = root/"gsm8k_train.jsonl"
+    if target_path.exists():
+        records = list(jsl.open(target_path))
+        print(f"found train set @:\n\t{str(target_path)}")
+        print(f"\t{records[0]=}")
+        print(f"\t{len(records)=}")
+    else: 
+        # download 
+        gsm_train = datasets.load_dataset('gsm8k', 'main')['train']
+
+        # parse
+        def parse_raw_target(answer_raw:str)-> str:
+            return answer_raw.split("### ")[-1].strip()
+        df = pd.DataFrame(gsm_train)
+        df['ans'] = df.answer.apply(parse_raw_target)
+        df['idx'] = df.index
+        records = df.to_dict(orient='records')
+        with jsl.open(target_path, 'w') as writer:
+            writer.write_all(records)
+            print(f'gsm8k train download and write: done.\n\t{str(target_path)}')
+    return str(target_path)
+
+
+def test_utils():
+    gsm8k_train_download_and_parse()
+    kshots=get_k_train_shots()
+    print(kshots)
+
+if __name__ == "__main__":
+    Fire(test_utils) 
+    
